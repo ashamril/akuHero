7bad015967f3be0028aca14f1248813b9890ee2c
Ami Shamril <ami@oc3460564501.ibm.com>
2021-04-11 12:41:07 +0800

    4th commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     print("Handling request to home page.")
-    return "Hello Beautiful World! This is a test!"
+    return "Hello World! This is a test 12345!"
 
 if __name__ == '__main__':
     app.run()
